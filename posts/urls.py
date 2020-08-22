e68409564769718cb74206aec096f1d6502f5b99
Elizaveta Bezyakina <e-bezyakina@yandex-team.ru>
2020-08-22 14:34:09 +0300

    add follow paths

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("follow/", views.follow_index, name="follow_index"),
     path("group/<slug:slug>/", views.group_posts, name="group_posts"),
     path("new/", views.new_post, name="new_post"),
     path("<str:username>/", views.profile, name="profile"),
@@ -15,5 +16,13 @@ urlpatterns = [
     ),
     path(
         "<str:username>/<int:post_id>/edit/", views.post_edit, name="post_edit"
+    ),
+    path(
+        "<str:username>/follow/", views.profile_follow, name="profile_follow"
+    ),
+    path(
+        "<str:username>/unfollow/",
+        views.profile_unfollow,
+        name="profile_unfollow",
     ),
 ]

2554e701db8d0c174fe6851ce6fbc30e663a336e
Liza Bezyakina <e.bezyakina@edadeal.ru>
2021-01-14 14:00:04 +0300

    add sentry

--- yatube/settings.py
+++ yatube/settings.py
@@ -17,6 +17,15 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    integrations=[DjangoIntegration()],
+)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
